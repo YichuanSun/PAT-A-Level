90568cbb738ff4ca86074a43d6b28c5bab7dc0aa
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-08-19 19:00:18 +0800

    .

--- 1034/main.cpp
+++ 1034/main.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
+typedef pair<string,string> suf;
+map<suf,int> graph;
+int main()  {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    suf tsuf;
+    int n,k,va;
+    string tes;
+    cin>>n>>k;
+    for (int i=0;i<n;i++)   {
+        cin>>tsuf.first>>tsuf.second>>va;
+        graph[suf]=va;
+        swap(tsuf.first,tsuf.second);
+        graph[suf]=va;
+    }
 
-int main()
-{
-    cout << "Hello world!" << endl;
     return 0;
 }
