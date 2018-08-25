862be525d97aaa757a4b9ffe8c008dff178af7ac
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-08-25 20:09:03 +0800

    tle

--- 1063/main.cpp
+++ 1063/main.cpp
@@ -1,16 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct st{
-    int data,fr;
-};
-vector<st> ss[52];
+typedef set<int>::iterator si;
+set<int> ss[55];
 int main()  {
-    int n,nu,t;
-    for (int i=0;i<n;i++)   {
-        cin>>nu;
-        for (int i=0;i<nu;i++)  {
-            cin>>
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n,nn,t,e1,e2;
+    cin>>n;
+    cout.setf(ios::fixed);
+    for (int i=1;i<=n;i++)   {
+        cin>>nn;
+        for (int j=0;j<nn;j++)  {
+            cin>>t;
+            ss[i].insert(t);
         }
+    }
+    cin>>nn;
+    for (int i=0;i<nn;i++)  {
+        cin>>e1>>e2;
+        int cnt=0;
+        for (si j=ss[e1].begin();j!=ss[e1].end();j++)  {
+            si it1=ss[e2].find(*j);
+            if (it1!=ss[e2].end())  cnt++;
+        }
+        int tmp=ss[e1].size()+ss[e2].size()-cnt;
+        double ans=1.0*cnt/tmp*100;
+        cout<<setprecision(1)<<ans<<'%'<<endl;
     }
     return 0;
 }
