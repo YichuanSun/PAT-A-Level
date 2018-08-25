45c702c7b90051c8a8ccf1d3171beb34f9a088d1
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-08-25 18:07:30 +0800

    ss

---
+++ 1063/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct st{
+    int data,fr;
+};
+vector<st> ss[52];
+int main()  {
+    int n,nu,t;
+    for (int i=0;i<n;i++)   {
+        cin>>nu;
+        for (int i=0;i<nu;i++)  {
+            cin>>
+        }
+    }
+    return 0;
+}
