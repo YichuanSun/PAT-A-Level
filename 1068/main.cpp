b29cbc32b534dfa12a36434ac91022605b287c18
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-08-28 16:46:38 +0800

    dfs,or dp

--- 1068/main.cpp
+++ 1068/main.cpp
@@ -1,36 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-int pre[10005],n,m,cnt;
-vector<int> ans[100];
-void dfs(int *a,int b,int m,int d);
+int a[10005],n,m,cnt,sum;
+bool ok;
+vector<int> ans;
+void dfs(int p,int m);
 int main()  {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int *a=new int[n];
     cin>>n>>m;
-    for (int i=0;i<n;i++)   cin>>a[i];
+    for (int i=0;i<n;i++)   {
+        cin>>a[i];
+        sum+=a[i];
+    }
+    if (sum<m)  {
+        cout<<"No Solution"<<endl;
+        return 0;
+    }
     sort(a,a+n);
-    for (int i=0;i<n;i++)   dfs(a,i,m,i);
+    dfs(0,m);
+    if (!ok)    cout<<"No Solution"<<endl;
     return 0;
 }
 
-void dfs(int *a,int b,int m,int d) {
+void dfs(int p,int m) {
+    //cout<<"Case:"<<cnt++<<endl;
+    //if (!ans.empty()&&ans[1]==3)    system("pause");
+    if (ok) return;
     if (m<0)    return;
-    if (m==0)   {
-        while (b!=d)    {
-            ans[cnt].push_back(a[b]);
-            b=pre[b];
-        }
-        ans[cnt].push_back(a[d]);
-        cnt++;
+    else if (m==0)   {
+        ok=true;
+        cout<<ans[0];
+        for (unsigned i=1;i<ans.size();i++)
+            cout<<' '<<ans[i];
+        cout<<endl;
         return;
     }
-    for (int i=b;i<n;i++) {
-        if (m-a[i]>=0)  {
-            pre[i]=b;
-            dfs(a,i,m-a[i],d);
-        }
+//    if (!ans.empty())   cout<<ans[0];
+//    for (unsigned i=1;i<ans.size();i++)
+//        cout<<' '<<ans[i];
+//    cout<<endl;
+    for (int i=p;i<n;i++)   {
+        ans.push_back(a[i]);
+        dfs(i+1,m-a[i]);
+        ans.erase(ans.end()-1);
     }
 }
-
-
