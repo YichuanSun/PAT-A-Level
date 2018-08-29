54908917dcfabe1ab87e99e43efbaaeca7c46624
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-08-29 20:30:47 +0800

    列表转置题

--- 1074/main.cpp
+++ 1074/main.cpp
@@ -1,41 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct node{
-    int now,data,nex;
-}nodes[100005],pp[100005];
+int node[100005][2],li[100005];
 int main()  {
-    int f,n,k;
-    node nnd;
-    cin>>f>>n>>k;
-    int ff=f;
+    int add,n,k,ad,da,ne;
+    scanf("%d%d%d",&add,&n,&k);
     for (int i=0;i<n;i++)   {
-        cin>>nnd.now>>nnd.data>>nnd.nex;
-        nodes[nnd.now]=nnd;
+        scanf("%d%d%d",&ad,&da,&ne);
+        node[ad][0]=da;
+        node[ad][1]=ne;
     }
-    for (int i=0;i<n;i++)   {
-        pp[i]=nodes[ff];
-        ff=nodes[ff].nex;
+    int pp=add,i=0;
+    while (pp!=-1)   {
+        li[i++]=pp;
+        pp=node[pp][1];
     }
-    for (int i=0;i<n;i+=k)   {
-        if (i+k>n) break;
-        reverse(pp+i,pp+i+k);
-        int tadd=pp[i].nex;
-        for (int j=i;j<i+k-1;j++) {
-            pp[j].nex=pp[j+1].now;
-        }
-        if (i+k+k>n)    pp[i+k-1].nex=tadd;
-        else    pp[i+k-1].nex=pp[i+k+k-1].now;
+    li[i]=-1;
+    int cc=0;
+    while(cc+k<=i) {
+        reverse(li+cc,li+cc+k);
+        cc+=k;
     }
-    for (int i=0;i<n;i++){
-        cout.fill('0');
-        cout.width(5);
-        cout<<pp[i].now<<' ';
-        cout<<pp[i].data<<' ';
-        if (pp[i].nex!=-1)  {
-            cout.fill('0');
-            cout.width(5);
-        }
-        cout<<pp[i].nex<<endl;
-    }
+    for (int j=0;j<i-1;j++)
+        printf("%05d %d %05d\n",li[j],node[li[j]][0],li[j+1]);
+    printf("%05d %d -1\n",li[i-1],node[li[i-1]][0]);
     return 0;
 }
