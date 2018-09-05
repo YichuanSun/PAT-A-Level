8be77baf1c7b4a36287688d6fa6167baa8f0e6e9
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-05 20:39:36 +0800

    改进复杂度之后少了一个常数，但是程序运行时间几乎不变，说明这不是复杂度的主要来源
    那么问题最有可能出在cin、cout上了。也有可能是结构体的排序没有整型二维数组快。我觉得问题最有可能是流输入输出上，以后能用标准就用标准输入输出，碰到string再用流

--- 1074/main.cpp
+++ 1074/main.cpp
@@ -5,7 +5,7 @@ struct node{
 }nodes[100005],pp[100005];
 int main()  {
     ios::sync_with_stdio(false);
-    cin.tie(0);
+    cin.tie(0),cout.tie(0);
     int f,n,k;
     node nnd;
     cin>>f>>n>>k;
@@ -24,14 +24,8 @@ int main()  {
     for (int i=0;i<nn;i+=k)   {
         if (i+k>nn) break;
         reverse(pp+i,pp+i+k);
-        int tadd=pp[i].nex;
-        for (int j=i;j<i+k-1;j++) {
-            pp[j].nex=pp[j+1].now;
-        }
-        if (i+k+k>nn)    pp[i+k-1].nex=tadd;
-        else    pp[i+k-1].nex=pp[i+k+k-1].now;
     }
-    for (int i=0;i<nn;i++){
+    for (int i=0;i<nn-1;i++){
         cout.fill('0');
         cout.width(5);
         cout<<pp[i].now<<' ';
@@ -40,58 +34,11 @@ int main()  {
             cout.fill('0');
             cout.width(5);
         }
-        cout<<pp[i].nex<<endl;
+        cout<<pp[i+1].now<<endl;
     }
+    cout.fill('0');
+    cout.width(5);
+    cout<<pp[nn-1].now;
+    cout<<' '<<pp[nn-1].data<<' '<<-1<<endl;
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//#include <bits/stdc++.h>
-//using namespace std;
-//int node[100005][2],li[100005];
-//int main()  {
-//    int add,n,k,ad,da,ne;
-//    scanf("%d%d%d",&add,&n,&k);
-//    for (int i=0;i<n;i++)   {
-//        scanf("%d%d%d",&ad,&da,&ne);
-//        node[ad][0]=da;
-//        node[ad][1]=ne;
-//    }
-//    int pp=add,i=0;
-//    while (pp!=-1)   {
-//        li[i++]=pp;
-//        pp=node[pp][1];
-//    }
-//    li[i]=-1;
-//    int cc=0;
-//    while(cc+k<=i) {
-//        reverse(li+cc,li+cc+k);
-//        cc+=k;
-//    }
-//    for (int j=0;j<i-1;j++)
-//        printf("%05d %d %05d\n",li[j],node[li[j]][0],li[j+1]);
-//    printf("%05d %d -1\n",li[i-1],node[li[i-1]][0]);
-//    return 0;
-//}
