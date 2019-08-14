3b693fc3f04290f72f1ce9257c7fbb1943624199
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-08-14 20:06:29 +0800

    25分钟结束战斗，树的算法的确只是几个基础算法的组合

---
+++ 1127/main.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define N 105
+using namespace std;
+typedef struct bt{
+    int data;
+    bt *lc,*rc;
+}bt;
+int post[N],in[N],cnt,c,n;
+vector<int> v[N];
+queue<bt*> qb;
+bt* postInbuildTree(int post[],int in[],int n)  {
+    if (n<=0)   return nullptr;
+    int i=0;
+    while (post[n-1]!=in[i]) i++;
+    bt* nw=new bt;
+    nw->data=in[i];
+    nw->lc=postInbuildTree(post,in,i);
+    nw->rc=postInbuildTree(post+i,in+i+1,n-i-1);
+    return nw;
+}
+void zigzagTraversal(bt* root)  {
+    if (root==nullptr) return;
+    bt *last=root,*T=root,*frt=nullptr;
+    qb.push(T);
+    while (!qb.empty()) {
+        frt=qb.front();qb.pop();
+        v[cnt].push_back(frt->data);
+        if (frt->lc)  qb.push(frt->lc);
+        if (frt->rc)  qb.push(frt->rc);
+        if (frt==last)  {
+            if (!(cnt&1))   reverse(v[cnt].begin(),v[cnt].end());
+            last=qb.back();
+            for (int i=0;i<(int)v[cnt].size();i++,c++)
+                printf("%d%c",v[cnt][i],c==n-1?'\n':' ');
+            cnt++;
+        }
+    }
+}
+void pr(bt* root)   {
+    if (root==nullptr)  return;
+    printf("%d ",root->data);
+    pr(root->lc);
+    pr(root->rc);
+}
+int main()  {
+    scanf("%d",&n);
+    for (int i=0;i<n;i++)   scanf("%d",&in[i]);
+    for (int i=0;i<n;i++)   scanf("%d",&post[i]);
+    bt* root=postInbuildTree(post,in,n);
+    //pr(root);
+    zigzagTraversal(root);
+    return 0;
+}
+
