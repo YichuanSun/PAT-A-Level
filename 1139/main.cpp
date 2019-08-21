267ca2e98fd0b62f7887e7d3585d0f135d4bce5c
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-08-21 21:13:18 +0800

    好菜呀，就对了一个样例

--- 1139/main.cpp
+++ 1139/main.cpp
@@ -2,7 +2,15 @@
 #define N 10005
 using namespace std;
 int g[N][N];
-vector<int> vg[N];
+vector<int> vg[N];  //两个都是，<0为女，>0为男
+typedef pair<int,int> pii;
+vector<pii> vpi;
+void findFriends(int a,int b);
+bool cmp(pii a,pii b)   {
+    if (a.first==b.first)
+        return a.second<b.second;
+    return a.first<b.first;
+}
 int main(){
     int n,m,k,a,b;
     cin>>n>>m;
@@ -17,7 +25,32 @@ int main(){
     cin>>k;
     for (int i=0;i<k;i++)   {
         cin>>a>>b;
+        findFriends(a,b);
+        vpi.clear();
 
     }
     return 0;
+}
+
+void findFriends(int a,int b)    {
+    int ta=abs(a);
+    int jd=(a*b<0?-1:1);   //jd为负则-1，为正则1
+    for (int i=0;i<(int)vg[ta].size();i++)   {
+        int k=abs(vg[ta][i]);
+        if (vg[ta][i]*a<0)  continue;//此后vgtai和a同号
+        for (int j=0;j<(int)vg[k].size();j++)    {
+            int p=abs(vg[k][j]);//如果1，4异号且2,3同号就跳过
+            if (jd==-1&&vg[ta][i]*vg[k][j]>0)    continue;
+            if (jd==1&&vg[ta][i]*vg[k][j]<0)    continue;
+            for (int e=0;e<(int)vg[p].size();e++)   {
+                if (vg[p][e]==b)    {
+                    vpi.push_back({k,p});
+                }
+            }
+        }
+    }
+    sort(vpi.begin(),vpi.end(),cmp);
+    cout<<vpi.size()<<endl;
+    for (int i=0;i<(int)vpi.size();i++)
+        cout<<vpi[i].first<<' '<<vpi[i].second<<endl;
 }
