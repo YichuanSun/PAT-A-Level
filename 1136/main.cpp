daf5e9cbe14a436adbe109e07a78ac06d915a5b1
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-08-22 22:03:11 +0800

    居然是个大数加法。。。没想到

---
+++ 1136/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()  {
+    string s,t;
+    int cnt=0;
+    cin>>s;
+    t=reverse(s.begin(),s.end());
+    while (t!=s&&cnt<10)   {
+        cout<<s<<" + "<<t<<" = ";
+        int
+    }
+    return 0;
+}
