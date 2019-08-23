5fff9c4c872c346874f1deb41b4ba9441351b75d
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-08-23 23:15:48 +0800

    大数加法

--- 1136/main.cpp
+++ 1136/main.cpp
@@ -1,14 +1,42 @@
 #include <bits/stdc++.h>
+#define N 1005
 using namespace std;
-
-int main()  {
+string add(string s1, string s2)
+{
+    string s = s1;
+    int carry = 0;
+    for (int i = s1.size() - 1; i >= 0; i--)
+    {
+        s[i] = (s1[i] - '0' + s2[i] - '0' + carry) % 10 + '0';
+        carry = (s1[i] - '0' + s2[i] - '0' + carry) / 10;
+    }
+    if (carry > 0)
+        s = "1" + s;
+    return s;
+}
+int main()
+{
     string s,t;
     int cnt=0;
     cin>>s;
-    t=reverse(s.begin(),s.end());
-    while (t!=s&&cnt<10)   {
+    t=s;
+    reverse(t.begin(),t.end());
+    while (t!=s&&cnt<10)
+    {
         cout<<s<<" + "<<t<<" = ";
-        int
+        string sm=add(s,t);
+        cout<<sm<<endl;
+        s=t=sm;
+        reverse(t.begin(),t.end());
+        cnt++;
     }
+    if (t==s)
+        cout<<t<<" is a palindromic number.\n";
+    else
+        cout<<"Not found in 10 iterations.\n";
     return 0;
 }
+
+
+
+
