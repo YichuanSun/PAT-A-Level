49f9596f2d379fc1a061b9afa32d0e8210f169bd
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-08-24 23:20:33 +0800

    日常犯菜

---
+++ 1010/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()  {
+    string n1,n2;
+    int tag,rad;
+    cin>>n1>>n2>>tag>>rad;
+    for (int i=2;i<=35;i++) {
+        string stoi()
+    }
+    return 0;
+}
