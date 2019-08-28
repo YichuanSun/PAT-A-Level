98d34462e477fab434063d2a83227275b7ffc5a1
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-08-28 21:45:18 +0800

    。

--- 1043/main.cpp
+++ 1043/main.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 #define N 1005
+#define MX 10000000
 using namespace std;
 struct bt{
     int data;
     bt *lc,*rc;
 };
-int a[N],mn=-10000000,mx=10000000,fg=0;
+int a[N],mn=-MX,mx=MX,fg=0;
 bt* buildTree(int a[],int n,bool cmp(int,int));
 bool cmpltoh(int a,int b)   {return a<=b;}
 bool cmphtol(int a,int b)   {return a>=b;}
@@ -20,34 +21,26 @@ bool jud(bt* t,bool cmp(int,int),int r) {
     if (t==nullptr) return true;
     bool b1,b2;
     b1=jud(t->lc,cmp,r);
-    //cout<<b1<<' '<<t->data<<endl;
     if (b1==false||(cmp(t->data,r)&&t->data!=r))  return false;
     r=t->data;
     b2=jud(t->rc,cmp,r);
     return b2;
 }
-
 int main()  {
     int n;
     cin>>n;
     for (int i=0;i<n;i++)   cin>>a[i];
     bt* root1=buildTree(a,n,cmpltoh);   //虽然结果一样，但是树建出来是错的
     bt* root2=buildTree(a,n,cmphtol);
-    if (jud(root1,cmpltoh,mn))  {
-        cout<<"YES"<<endl;post(root1);cout<<endl;
-    }
-    else if (jud(root2,cmphtol,mx))   {
-        cout<<"YES"<<endl;post(root2);cout<<endl;
-    }
+    if (jud(root1,cmpltoh,mn))  {cout<<"YES"<<endl;post(root1);cout<<endl;}
+    else if (jud(root2,cmphtol,mx))   {cout<<"YES"<<endl;post(root2);cout<<endl;}
     else cout<<"NO"<<endl;
     return 0;
 }
-
 bt* buildTree(int a[],int n,bool cmp(int,int))    {
     if (n<=0)    return nullptr;
     int i=0;
     while (cmp(a[i],a[0])&&i<n)  i++;    //i-1是子树节点数
-    //cout<<i<<endl;
     bt* nw= new bt;
     nw->data=a[0];
     nw->lc=buildTree(a+1,i-1,cmp);
