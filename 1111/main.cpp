1eacd02d7b83bd2cb7360c2db37c1b7ed5f61d86
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-08-31 20:45:26 +0800

    看着简单，实操一些细节还是很麻烦的

--- 1111/main.cpp
+++ 1111/main.cpp
@@ -3,7 +3,7 @@
 #define INF 100000005
 using namespace std;
 typedef pair<int,int> pii;
-pii g[N][N];
+pii g[N][N];    //错因1，切记，易忘：初始化须将每条边设为INF
 int n,m,d[N],dispred[N],t[N],nodenum[N],timepred[N];
 bool used[N];
 void dijkstraDis(int be);
@@ -11,19 +11,19 @@ void dijkstraTime(int be);
 vector<int> getPath(int a[],int ed);
 int main()  {
     pii tp={INF,INF};
-    fill(g[0],g[0]+N*N,tp);
-    int v1,v2,l,ti,sp,be,en;
+    fill(g[0],g[0]+N*N,tp);     //对图g的初始化
+    int v1,v2,l,ti,sp,be,en;    //中间变量
     cin>>n>>m;
     for (int i=0;i<m;i++)   {
         cin>>v1>>v2>>sp>>l>>ti;
         g[v1][v2]={l,ti};
-        if (sp==0)  g[v2][v1]={l,ti};
+        if (sp==0)  g[v2][v1]={l,ti};   //初始化双向边
     }
-    cin>>be>>en;
-    dijkstraDis(be);
-    dijkstraTime(be);
-    vector<int> dip=getPath(dispred,en);
-    vector<int> tip=getPath(timepred,en);
+    cin>>be>>en;                //起点 终点
+    dijkstraDis(be);            //对距离进行最短路
+    dijkstraTime(be);           //对时间进行最短路
+    vector<int> dip=getPath(dispred,en);    //路径还原的最短距离
+    vector<int> tip=getPath(timepred,en);   //路径还原的最短时间
     printf("Distance = %d",d[en]);
     if (dip==tip)   printf("; Time = %d: ",t[en]);
     else {
@@ -42,7 +42,7 @@ int main()  {
     return 0;
 }
 
-void dijkstraDis(int be)   {
+void dijkstraDis(int be)   {        //条件最短路，就需要开一个辅助数组记录每个节点条件的值，一般一维数组足矣
     fill(d,d+N,INF);
     fill(used,used+N,false);
     fill(dispred,dispred+N,-2);
