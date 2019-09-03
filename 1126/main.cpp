6629a5fed09eec55bf982891b732494f9d59f4ff
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-09-03 20:33:46 +0800

    原来是忘了判断连通性，只有联通图才可能为欧拉图

--- 1126/main.cpp
+++ 1126/main.cpp
@@ -1,25 +1,41 @@
 #include <bits/stdc++.h>
 #define N 505
 using namespace std;
-int g[N][N];
-vector<int> v[N];
-int idg[N],odg[N];
+int odg[N];
+bool usd[N];
+vector<int> g[N];
+void dfs(int p);
 int main()  {
     int n,m,a,b,odd=0;
     cin>>n>>m;
     for (int i=0;i<m;i++)   {
         cin>>a>>b;
-        g[a][b]=1;
-        odg[a]++;
-        idg[b]++;
+        g[a].push_back(b);
+        g[b].push_back(a);
+        odg[a]++;odg[b]++;
     }
     for (int i=1;i<=n;i++)  {
-        int dg=odg[i]+idg[i];
+        int dg=odg[i];
         printf("%d%c",dg,i==n?'\n':' ');
         if ((dg&1)==1)  odd++;
+    }
+    dfs(1);
+    for (int i=1;i<=n;i++)   {
+        if (!usd[i])    {
+            cout<<"Non-Eulerian"<<endl;
+            return 0;
+        }
     }
     if (odd==0) cout<<"Eulerian"<<endl;
     else if (odd==2)    cout<<"Semi-Eulerian"<<endl;
     else cout<<"Non-Eulerian"<<endl;
     return 0;
 }
+
+void dfs(int p) {
+    if (usd[p]) return;
+    usd[p]=true;
+    for (int i=0;i<(int)g[p].size();i++)
+        dfs(g[p][i]);
+}
+
