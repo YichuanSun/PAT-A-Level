3f7942fe2f9c7dea84084ce6305fa1834045f177
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-09-03 17:40:58 +0800

    不用STL花了一个多小时才拿了70%的分，用STL不用半小时就能满分。。。STL很多细节都忘了。。。题海战术不服不行啊

--- 1129/main.cpp
+++ 1129/main.cpp
@@ -1,9 +1,48 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+#define N 50005
 using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
+int a[N],frq[N],usd[N];
+vector<int> vr;
+bool cmp(int a,int b)   {
+    if (frq[a]==frq[b]) return a<b;
+    return frq[a]>frq[b];
+}
+int main()  {
+    int n,k,sml=-1;
+    cin>>n>>k;
+    for (int i=0;i<n;i++)   {
+        cin>>a[i];
+        if (i==0)   {continue;}
+        frq[a[i-1]]++;
+        if ((int)vr.size()<k)    {   //如果序列未满
+            if (usd[a[i-1]]==0)   {   //如果序列中无该值
+                vr.push_back(a[i-1]);
+                usd[a[i-1]]=1;
+            }
+            else {
+                sort(vr.begin(),vr.end(),cmp);
+                if (!vr.empty())    sml=frq[vr.back()];
+            }
+        }
+        else {  //序列已满
+            if (usd[a[i-1]]==0)   {   //如果序列中无该值
+                if (frq[a[i-1]]>=sml)  {   //并且这个值出现频数大于等于序列中的最小值
+                    vr.push_back(a[i-1]);
+                    sort(vr.begin(),vr.end(),cmp);
+                    if (vr.back()!=a[i-1])    {usd[a[i-1]]=1;usd[vr.back()]=0;}
+                    vr.pop_back();
+                    if (!vr.empty())    sml=frq[vr.back()];
+                }
+            }
+            else {  //如果有该值
+                sort(vr.begin(),vr.end(),cmp);
+                if (!vr.empty()) sml=frq[vr.back()];
+            }
+        }
+        cout<<a[i]<<':';
+        for (int j=0;j<(int)vr.size();j++)
+            cout<<' '<<vr[j];
+        cout<<endl;
+    }
     return 0;
 }
