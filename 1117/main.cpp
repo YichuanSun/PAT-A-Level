8b5280e2a76baf61f69abedbcf351813df3504eb
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-09-05 16:24:23 +0800

    ???

---
+++ 1117/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define N 100005
+using namespace std;
+int a[N];
+bool usd[N]
+int main(){
+    int n,fl=N,ce=-1,frq=-1,ans=0;
+    scanf("%d",&n);
+    for (int i=0;i<n;i++)   {
+        scanf("%d",a+i);
+        fl=min(fl,a[i]);
+        ce=max(ce,a[i]);
+    }
+    for (int i=0;i<n;i++)   {
+
+    }
+    return 0;
+}
