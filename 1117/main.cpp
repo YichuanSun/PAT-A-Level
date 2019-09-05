f9b40b945cbc3b7f7a931b9a5cf24b5acf06b763
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-09-05 17:58:17 +0800

    ?

--- 1117/main.cpp
+++ 1117/main.cpp
@@ -1,18 +1,25 @@
 #include <bits/stdc++.h>
 #define N 100005
 using namespace std;
-int a[N];
-bool usd[N]
+int a[N],b[N],id[N];
+bool usd[N];
+bool cmp(int a,int b)   {return a>b;}
 int main(){
-    int n,fl=N,ce=-1,frq=-1,ans=0;
+    int n,c=0,ans=0;
     scanf("%d",&n);
     for (int i=0;i<n;i++)   {
         scanf("%d",a+i);
-        fl=min(fl,a[i]);
-        ce=max(ce,a[i]);
     }
+    sort(a,a+n,cmp);
     for (int i=0;i<n;i++)   {
-
+        if (!usd[a[i]]) {
+            b[c++]=a[i];
+            id[a[i]]=i;
+        }
+        usd[a[i]]=1;
     }
+    for (int i=0;i<c;i++)
+        if (b[i]>=id[b[i]])  ans=max(ans,id[b[i]]);
+    cout<<ans<<endl;
     return 0;
 }
